12bea89c7a52009ecf7636299011bcd0eecd2708
mayankclaux-master <mayankclaux@gmail.com>
2026-04-30 19:30:41 +0530

    fix: add pkce flow type to provisioning page

--- apps/web/app/onboarding/provisioning/page.tsx
+++ apps/web/app/onboarding/provisioning/page.tsx
@@ -35,6 +35,9 @@ export default async function ProvisioningPage() {
     normalizeSupabaseUrl(process.env.NEXT_PUBLIC_SUPABASE_URL!),
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      auth: {
+        flowType: 'pkce',
+      },
       cookies: {
         getAll() { return cookieStore.getAll() },
         setAll(cookiesToSet: any[]) {
